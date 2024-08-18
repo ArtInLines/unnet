89db7b51c1ecb212c14a21ab75fc95e7b3a1d0c0
ArtInLines <linesinart@gmail.com>
2024-08-18 15:50:58 +0200

    implement minimal network analysis

--- unnet.py
+++ unnet.py
@@ -1,16 +1,20 @@
+from typing import Tuple
+from time   import time
+from sys    import argv
 import smolXML as xml
 import os
-import time
 
 class LOG:
 	INFO = 0
 	WARN = 1
 	ERR  = 2
-	SUCC = 3
+	DATA  = 3
+	DBG  = 4
 	STR_INFO = '\033[37m[INFO]: '
 	STR_WARN = '\033[33m[WARN]: '
 	STR_ERR  = '\033[31m[ERR]:  '
-	STR_SUCC = '\033[32m[SUCC]: '
+	STR_DATA = '\033[32m[DATA]: '
+	STR_DBG  = '\033[32m[DBG]:  '
 	COL_DONE = '\033[0m'
 
 	@staticmethod
@@ -21,8 +25,10 @@ class LOG:
 			s = LOG.STR_WARN + s + LOG.COL_DONE
 		elif log_level == LOG.ERR:
 			s = LOG.STR_ERR  + s + LOG.COL_DONE
-		elif log_level == LOG.SUCC:
-			s = LOG.STR_SUCC + s + LOG.COL_DONE
+		elif log_level == LOG.DATA:
+			s = LOG.STR_DATA + s + LOG.COL_DONE
+		elif log_level == LOG.DBG:
+			s = LOG.STR_DBG  + s + LOG.COL_DONE
 		else:
 			print(f"log() received unexpected value for log_level: {log_level}".ljust(max_line))
 			assert(False)
@@ -42,32 +48,32 @@ def log(log_level: int, s: str, new_line: bool = True):
 		endc = '\r'
 	print(s, end=endc)
 
-def create_network(csv_path: str):
-	csv = open(csv_path, "w", encoding="utf8")
-	csv.write("from,to\n")
+def create_network(data_dir: str, out_csv_path: str):
+	log(LOG.INFO, "Creating Network from data files in {data_dir}...")
+	csv = open(out_csv_path, "wb")
+	csv.write("from,to\n".encode("utf8"))
 
 	files = []
-	p1 = "data"
-	for d1 in os.listdir(p1):
-		p2 = os.path.join(p1, d1)
-		if not os.path.isdir(p2):
+	for d1 in os.listdir(data_dir):
+		p1 = os.path.join(data_dir, d1)
+		if not os.path.isdir(p1):
 			continue
-		for d2 in os.listdir(p2):
-			p3 = os.path.join(p2, d2)
-			if not os.path.isdir(p2):
+		for d2 in os.listdir(p1):
+			p2 = os.path.join(p1, d2)
+			if not os.path.isdir(p1):
 				continue
-			for f in os.listdir(p3):
-				fpath = os.path.join(p3, f)
+			for f in os.listdir(p2):
+				fpath = os.path.join(p2, f)
 				if fpath.endswith(".xml"):
 					files.append(fpath)
 
 	files_wo_edges = []
 	edge_count     = 0
 	files_count    = len(files)
-	log(LOG.INFO, f"Found {files_count} XML files")
+	log(LOG.DATA, f"Found {files_count} XML files")
 
 	for i in range(files_count):
-		log(LOG.INFO, f"{100*(i/files_count):.3f}% done | {edge_count} edges found", False)
+		log(LOG.DATA, f"{100*(i/files_count):.3f}% done | {edge_count} edges found", False)
 		fpath = files[i]
 		root = xml.parseFile(fpath)
 		docNums = root.getAllElementsOfType("docNumber")
@@ -91,22 +97,83 @@ def create_network(csv_path: str):
 				# log(LOG.WARN, f"Found link referring outside of undocs.org in {fpath}: '{link}'")
 				continue
 			other = "".join(link_parts[1].split("en/")).split("(")[0]
-			csv.write(f"{docNum},{other}\n")
+			csv.write(f"{docNum},{other}\n".encode("utf8"))
 			edge_count += 1
 		if edge_count == initial_edge_count:
 			files_wo_edges.append(fpath)
 	if len(files_wo_edges) > 0:
 		log(LOG.WARN, f"{len(files_wo_edges)} files found without outgoing edges")
 	csv.close()
-	log(LOG.INFO, f"{edge_count} edges successfully written to {csv_path}")
+	log(LOG.DATA, f"{edge_count} edges successfully written to {out_csv_path}")
+
+
+class Node():
+	incoming: int
+	outgoing: int
+	def __init__(self, incoming: int, outgoing: int):
+		self.incoming = incoming
+		self.outgoing = outgoing
+
+def analyze_network(csv_path: str):
+	log(LOG.INFO, "Analyzing Network stored in {csv_path}...")
+	net: dict[str, Node] = {}
+	with open(csv_path, "r", encoding="utf8") as csv:
+		lines = csv.read().splitlines()[1:] # Ignore header
+		for line in lines:
+			src, dst = line.split(",")
+			if src in net:
+				net[src].outgoing += 1
+			else:
+				net[src] = Node(0, 1)
+			if dst in net:
+				net[dst].incoming += 1
+			else:
+				net[dst] = Node(1, 0)
+	log(LOG.DATA, f"{len(net)} different nodes found")
+
+	only_incoming_count = 0
+	only_outgoing_count = 0
+	both_edges_count = 0
+	incoming_max = 0
+	outgoing_max = 0
+	degrees_max  = 0
+	max_incoming_node: str
+	max_outgoing_node: str
+	max_degrees_node:  str
+	for (node, val) in net.items():
+		if val.incoming == 0:
+			only_outgoing_count += 1
+		elif val.outgoing == 0:
+			only_incoming_count += 1
+		else:
+			both_edges_count += 1
+		if val.incoming > incoming_max:
+			incoming_max = val.incoming
+			max_incoming_node = node
+		if val.outgoing > outgoing_max:
+			outgoing_max = val.outgoing
+			max_outgoing_node = node
+		if val.incoming + val.outgoing > degrees_max:
+			degrees_max = val.incoming + val.outgoing
+			max_degrees_node = node
+	log(LOG.DATA, f"{only_incoming_count} nodes without outgoing connections")
+	log(LOG.DATA, f"{only_outgoing_count} nodes without incoming connections")
+	log(LOG.DATA, f"{both_edges_count} nodes with incoming & outgoing connections")
+	log(LOG.DATA, f"Node with most incoming connections: '{max_incoming_node}' ({incoming_max})")
+	log(LOG.DATA, f"Node with most outgoing connections: '{max_outgoing_node}' ({outgoing_max})")
+	log(LOG.DATA, f"Node with highest degree: '{max_degrees_node}' ({degrees_max})")
 
 
 def main():
-	start_time = time.time()
-	csv_path: str = os.path.join("data", "edges.csv")
-	create_network(csv_path)
+	start_time = time()
 
-	elapsed = time.time() - start_time
+	data_dir: str = "./data"
+	csv_path: str = os.path.join(data_dir, "edges.csv")
+	if not os.path.exists(csv_path):
+		create_network(data_dir, csv_path)
+	analyze_network(csv_path)
+
+	elapsed = time() - start_time
 	if elapsed > 60:
 		minutes = int(elapsed/60)
 		log(LOG.INFO, f"Program took {minutes}m {(elapsed - minutes*60):.3f}s")
