965cdf9471ca2f660786aea4711d9ad1b264a4e9
ArtInLines <linesinart@gmail.com>
2024-08-18 13:41:01 +0200

    improve setup

--- setup.py
+++ setup.py
@@ -4,4 +4,9 @@ from Cython.Build import cythonize
 setup(
     name='unnet',
     ext_modules=cythonize("unnet.py"),
+    version="0.0.1",
+    description="Network analysis for UN resolutions",
+    author="ArtInLines",
+    url="https://github.com/artInLines/unnet",
+    license="MIT"
 )
