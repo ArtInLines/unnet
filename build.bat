35a04ce0e5b2645392ccb47036f19983fc3109f8
ArtInLines <linesinart@gmail.com>
2024-08-18 15:57:29 +0200

    remove cython dependency

--- build.bat
+++
@@ -1,8 +0,0 @@
-:: Typecheck
-@echo Typechecking...
-@call mypy unnet.py --check-untyped-defs
-
-:: Build & install as python package
-@echo Building...
-@call python setup.py build
-@call python setup.py install
--- setup.bat
+++
@@ -1,7 +0,0 @@
-:: Setup virtual environment
-@echo Setting up environment
-@call venv\Scripts\activate.bat
-
-:: Install dependencies
-@echo Installing dependencies...
-@call pip install -r requirements.txt
