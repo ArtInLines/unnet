e361bd09e9a74e5bb56f549f2db5853b438e5012
ArtInLines <64645307+ArtInLines@users.noreply.github.com>
2024-09-12 18:38:33 +0200

    collect voting data in a table

--- unnet.py
+++ unnet.py
@@ -48,11 +48,8 @@ def log(log_level: int, s: str, new_line: bool = True):
 		endc = '\r'
 	print(s, end=endc)
 
-def create_network(data_dir: str, out_csv_path: str):
-	log(LOG.INFO, "Creating Network from data files in {data_dir}...")
-	csv = open(out_csv_path, "wb")
-	csv.write("from,to\n".encode("utf8"))
 
+def get_files(data_dir: str) -> [str]:
 	files = []
 	for d1 in os.listdir(data_dir):
 		p1 = os.path.join(data_dir, d1)
@@ -66,11 +63,26 @@ def create_network(data_dir: str, out_csv_path: str):
 				fpath = os.path.join(p2, f)
 				if fpath.endswith(".xml"):
 					files.append(fpath)
+	log(LOG.DATA, f"Found {len(files)} XML files")
+	return files
 
+
+class Node():
+	incoming: int
+	outgoing: int
+	def __init__(self, incoming: int, outgoing: int):
+		self.incoming = incoming
+		self.outgoing = outgoing
+
+def create_network(data_dir: str, out_csv_path: str):
+	log(LOG.INFO, f"Creating Network from data files in {data_dir}...")
+	csv = open(out_csv_path, "wb")
+	csv.write("from,to\n".encode("utf8"))
+
+	files          = get_files(data_dir)
 	files_wo_edges = []
 	edge_count     = 0
 	files_count    = len(files)
-	log(LOG.DATA, f"Found {files_count} XML files")
 
 	for i in range(files_count):
 		log(LOG.DATA, f"{100*(i/files_count):.3f}% done | {edge_count} edges found", False)
@@ -106,15 +118,11 @@ def create_network(data_dir: str, out_csv_path: str):
 	csv.close()
 	log(LOG.DATA, f"{edge_count} edges successfully written to {out_csv_path}")
 
+def analyze_network(data_dir: str, csv_file_name: str, always_recreate_network: bool):
+	csv_path: str = os.path.join(data_dir, csv_file_name)
+	if always_recreate_network or not os.path.exists(csv_path):
+		create_network(data_dir, csv_path)
 
-class Node():
-	incoming: int
-	outgoing: int
-	def __init__(self, incoming: int, outgoing: int):
-		self.incoming = incoming
-		self.outgoing = outgoing
-
-def analyze_network(csv_path: str):
 	log(LOG.INFO, f"Analyzing Network stored in {csv_path}...")
 	net: dict[str, Node] = {}
 	with open(csv_path, "r", encoding="utf8") as csv:
@@ -164,14 +172,112 @@ def analyze_network(csv_path: str):
 	log(LOG.DATA, f"Node with highest degree: '{max_degrees_node}' ({degrees_max})")
 
 
-def main():
-	start_time = time()
+# Gets the index of the column in the table
+# If the column doesn't exist, it is added
+def col_idx_insert(rows: [[int]], cols: [any], col: any, null_value: any = "") -> int:
+	for i in range(len(cols)):
+		if cols[i] == col:
+			return i
+	cols.append(col)
+	for i in range(len(rows)):
+		rows[i].append(null_value)
+	return len(cols) - 1
 
+def create_voting_table(data_dir: str, csv_file_name: str):
+	log(LOG.INFO, f"Creating voting table from data files in {data_dir}...")
+	csv_path: str = os.path.join(data_dir, csv_file_name)
+	cols = ["date"]
+	rows = []
+
+	no_votes_count = 0
+	files          = get_files(data_dir)
+	files_count    = len(files)
+	for i in range(files_count):
+		log(LOG.DATA, f"{100*(i/files_count):.3f}% done | {100*((i - no_votes_count)/files_count):.3f}% files with votes | {len(cols)} countries", i+1 == files_count)
+		fpath = files[i]
+		root  = xml.parseFile(fpath)
+
+		date  = None
+		dates = root.getAllElementsOfType("FRBRdate")
+		for d in dates:
+			if "name" in d.attrs and d.attrs["name"] == "adoption" and "date" in d.attrs:
+				date = d.attrs["date"]
+				break
+		if date is None:
+			if len(dates) > 0 and "date" in dates[0].attrs:
+				log(LOG.WARN, f"No 'adoption'-date found in file {fpath} - using '{dates[0].name}' date instead")
+				date = dates[0].attrs["date"]
+			else:
+				log(LOG.WARN, f"No date found in file {fpath}")
+				date = ""
+
+		votings = root.getAllElementsOfType("voting")
+		if len(votings) == 0:
+			log(LOG.WARN, f"No voting block in file {fpath}")
+			no_votes_count += 1
+		elif len(votings) > 1:
+			log(LOG.WARN, f"{len(votings)} voting blocks in file {fpath}")
+			no_votes_count += 1
+		elif "outcome" not in votings[0].attrs or votings[0].attrs["outcome"] != "adoptedRecordedVote":
+			no_votes_count += 1
+		else:
+			counts = votings[0].getAllElementsOfType("count")
+			if False and len(counts) != 3:
+				log(LOG.WARN, f"Expected to find 3 count blocks in voting block, but '{fpath}' contains {len(counts)} count blocks instead")
+				no_votes_count += 1
+			else:
+				rows.append([date])
+				for _ in cols:
+					rows[-1].append("")
+
+				for count in counts:
+					val: int
+					if "refersTo" not in count.attrs:
+						log(LOG.ERR, f"No 'refersTo' attribute in count block in file {fpath}")
+						os.abort()
+					if count.attrs["refersTo"] == 'inFavour':
+						val = "1"
+					elif count.attrs["refersTo"] == 'against':
+						val = "-1"
+					elif count.attrs["refersTo"] == 'abstaining':
+						val = "0"
+					else:
+						log(LOG.ERR, f"Invalid value '{count.attrs["refersTo"]}' for 'refersTo' attribute in count block in file {fpath}")
+						os.abort()
+
+					for org in count.getAllElementsOfType("organization"):
+						orgname = org.getStrVal()
+						idx     = col_idx_insert(rows, cols, orgname)
+						rows[-1][idx] = val
+	csv = open(csv_path, "wb")
+	csv.write(",".join(cols).encode("utf8"))
+	for row in rows:
+		csv.write(("\n" + ",".join(row)).encode("utf8"))
+	csv.close()
+
+
+
+
+def main(args: [str]):
 	data_dir: str = "data"
-	csv_path: str = os.path.join(data_dir, "edges.csv")
-	if not os.path.exists(csv_path):
-		create_network(data_dir, csv_path)
-	analyze_network(csv_path)
+
+	valid_arguments = ["help", "net", "vote"]
+	if len(args) == 0:
+		args = ["help"]
+	elif len(args) != 1 or args[0] not in valid_arguments:
+		log(LOG.ERR, "Invalid usage: You need to supply one of the following arguments: " + ", ".join(valid_arguments))
+	arg = args[0]
+
+	start_time = time()
+	if arg == "help":
+		print("The following options are provided by this program")
+		print("* 'help': Show this help message")
+		print("* 'net':  Collect the relations between the files as a network and do some simple analysis on it")
+		print("* 'vote': Collect all votes and store them as a matrix")
+	elif arg == "net":
+		analyze_network(data_dir, "edges.csv", True)
+	elif arg == "vote":
+		create_voting_table(data_dir, "voting.csv")
 
 	elapsed = time() - start_time
 	if elapsed > 60:
@@ -182,4 +288,4 @@ def main():
 
 
 if __name__ == "__main__":
-	main()
+	main(argv[1:])
